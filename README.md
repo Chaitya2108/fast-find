37fd0c125b634727f86d314afff6165877f27169
Sean Yen (UCSD work laptop) <ussa.say@gmail.com>
2025-04-26 13:36:54 -0700

    initial commit (untested)

---
+++ README.md
@@ -0,0 +1,1 @@
+scrape UCSD events on instagram
---
+++ scraper.ts
@@ -0,0 +1,11 @@
+import playwright from "playwright";
+
+// https://oxylabs.io/blog/playwright-web-scraping
+// Firefox: https://www.reddit.com/r/webscraping/comments/149czf4/how_i_can_bypass_antibot_in_playwright_or_seleium/jo5xhw2/
+const browser = await playwright.firefox.launch();
+const context = await browser.newContext();
+const page = await context.newPage();
+await page.goto("https://amazon.com");
+await page.screenshot({ path: "bruh.png", fullPage: true });
+await page.waitForTimeout(1000);
+await browser.close();
